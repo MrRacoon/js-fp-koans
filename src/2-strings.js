64a1b293771c783834aeb74f57f8d11ced28a308
Erik Sutherland <erik.misu@gmail.com>
2017-07-22 11:46:01 -0700

    add a second file "strings"

---
+++ src/2-strings.js
@@ -0,0 +1,23 @@
+import {join, concat} from 'ramda';
+// import __ from './internal/placeholder';
+
+describe('2-Strings', () => {
+  describe('The "join" function', () => {
+    it('will join strings together, interspersing the first argument', () => {
+      join(',', ['thing', 'other thing'])
+        .should.be.eql('thing,other thing');
+    });
+  });
+
+  describe('The "concat" function', () => {
+    it('only accepts two arguments', () => {
+      concat('1', '2', '3').should.be.eql('12');
+    });
+    it('joins two strings together with no interspersing', () => {
+      concat('thing', 'other thing').should.be.eql('thingother thing');
+    });
+    it('is curried', () => {
+      concat('1')('2').should.be.eql('12');
+    });
+  });
+});
